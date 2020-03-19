e455408a83ae596e060a4c70f673ae4bf470bcb6
Cassie051 <kasiaczarna051@gmail.com>
2020-03-19 01:12:23 +0100

    Add photo-template and footer

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def render_static_html():
-    return render_template('base.html')
+    pics = os.listdir('static/img/')
+    return render_template('index.html', name='Jan Kowalski', pics=pics)
+
+#@app.route('/about')
+#def about():
+#    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
     app.run(debug==True)
-
