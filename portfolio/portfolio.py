ca80f91a886a2bd11ff85de2fdfc87a6ea74019c
Cassie051 <kasiaczarna051@gmail.com>
2020-03-24 20:02:32 +0100

    Base for About and Contact

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def render_static_html():
+def home():
     pics = os.listdir('static/img/')
     return render_template('index.html', name='Jan Kowalski', pics=pics)
 
-#@app.route('/about')
-#def about():
-#    return render_template('about.html', title='About')
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', title='Contact')
 
 if __name__ == "__main__":
     app.run(debug==True)
