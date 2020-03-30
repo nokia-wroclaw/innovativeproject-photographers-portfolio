ca99b5aa56e32698ce8edd7c6a5def757165245a
ABielecka <blank.anna42@gmail.com>
2020-03-31 00:19:14 +0200

    Placeholder login/register page and makeshift registration system

---
+++ portfolio/Test/main1.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Form, Body
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+from pydantic import BaseModel
+
+class User(BaseModel):
+    imie: str
+    nazwisko: str
+    email: str
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def read_item(request: Request):
+    return templates.TemplateResponse("item.html", {"request": request })
+
+#@app.post("/user/", response_model=User)
+#async def login(username: User):
+#    return username
+
+@app.post("/user/")    
+async def login(*, imie: str = Form(...), nazwisko: str = Form(...), email: str = Form(...)):
+    return {"imie": imie, "   nazwisko": nazwisko, "   email":email}
+    
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- portfolio/server.py
+++ portfolio/server.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from forms import ContactForm
 import os
 
 app = FastAPI()
@@ -23,13 +22,18 @@ async def about(request: Request):
         "about.html", {"request": request, "title": "About", "name": "Jan Kowalski"}
     )
 
-
-@app.get("/contact")
-async def contact(request: Request):
-    form = ContactForm()
-    if form.validate_on_submit():
-        person = form.person.data
-        form.person.data = ''
+@app.get("/login")
+async def sign_in(request: Request):
     return templates.TemplateResponse(
-        'contact.html',{"request": request, "title":"Contact", "name":"Jan Kowalski", "form":form}
+        "login.html", {"request": request, "title": "Login"}
     )
+
+@app.get("/register")
+async def sign_up(request: Request):
+    return templates.TemplateResponse(
+        "register.html", {"request": request, "title": "Register"}
+    )
+
+@app.post("/register")
+async def create_user(*, fname: str = Form(...), lname: str = Form(...), email: str = Form(...)):
+    return {"fname": fname, "lname": lname, "email": email}
