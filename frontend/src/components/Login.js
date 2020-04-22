374c416902e0d9928f5af581e9c6d7bd9d9bcd16
AlutkaG <alagerlach@interia.pl>
2020-04-22 18:31:37 +0200

    Add fetch() json, but it doesn't work. We must change fetch url.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -10,19 +10,42 @@ import {
 import Register from './Register';
 
 class Login extends Component{
-  render(){
-    return(
+  constructor(props){
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state={
+      email: "",
+      password: ""
+    }
+  }
 
-        <Form className="login-form">
-        <div class="box">
-          <h1 class="signIn">Sign In</h1>
+  handleSubmit = e => {
+    e.preventDefault()
+    fetch('localhost:3000', {
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+       "email": this.state.email,
+       "password": this.state.password
+      })
+    })
+    .then(response => response.json())
+    .then(data => this.setState({postId:data.id}));
+  }
+
+  render(){
+    
+    return(
+        <Form className="login-form" onSubmit={this.handleSubmit}>
+        <div className="box">
+          <h1 className="signIn">Sign In</h1>
         <FormGroup style={{paddingBottom:'5%', paddingTop:'5%'}}>
-          <Input type="email" placeholder="Email"/>
+          <Input value={this.state.email} onChange={(ev)=>this.setState({email:ev.target.value})} type="email" placeholder="Email"/>
         </FormGroup>
         <FormGroup style={{paddingBottom:'5%'}}>
-          <Input type="password" placeholder="Password"/>
+          <Input value={this.state.password} onChange={(ev)=>this.setState({password:ev.target.value})} type="password" placeholder="Password"/>
         </FormGroup>
-        <Button className="btn-lg btn-dark btn-block">
+        <Button type="submit" className="btn-lg btn-dark btn-block">
           Log in
         </Button>
         <div className="text-center pt-3" style={{color:'white'}}>
@@ -32,7 +55,7 @@ class Login extends Component{
         <div className="text-center">
           <a href="/register" className="btn btn-dark" role="button">Sign Up</a>
           <span className="p-2"></span>
-          <a href="/forgot-password" class="btn btn-dark" role="button">Forgot Password</a>
+          <a href="/forgot-password" className="btn btn-dark" role="button">Forgot Password</a>
         </div>
         <div className="text-center" style={{paddingTop:'5%', color:"white"}}>
         By clicking “Sign up”, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Statement</a>. 
