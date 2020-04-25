cf6efbf59ba57c9a6d8d101cb659c6cb458ab160
AlutkaG <alagerlach@interia.pl>
2020-04-26 00:16:28 +0200

    Clearing-up in code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
-import Footer from './components/Footer'
 import Login from './components/Login'
-import Register from './components/Register'
+
 class App extends Component {
   render(){
     return (
       <div>
-        <h1  className="header" >Photographer's portfolio</h1>
         <Login />
       </div>
     );
