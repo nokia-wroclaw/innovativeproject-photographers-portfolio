e9ad407fe588bc01b899a15361d2d453e0876b12
Cassie051 <kasiaczarna051@gmail.com>
2020-04-29 00:02:33 +0200

    try log post

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -5,43 +5,37 @@ import { Link } from 'react-router';
 import Register from './Register';
 
 class Login extends Component{
-  constructor(props){
-    super(props)
+  constructor() {
+    super();
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state={
-      email: "",
-      password: ""
-    }
-  }
+}
 
-  handleSubmit = e => {
-    e.preventDefault()
-    fetch('/', {
-      method: 'post',
-      headers: {'Content-Type':'application/json'},
-      body: JSON.stringify({
-       "email": this.state.email,
-       "password": this.state.password
-      })
-    })
-    .then(response => response.json())
-    .then(data => this.setState({postId:data.id}));
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch('/login', {
+        method: 'POST',
+        body: data,
+    });
   }
 
   render(){
-    
+
     return(
       <Container className="bkgd" fluid>
+        <title> Login </title>
       <h1  className="header" >Photographer's portfolio</h1>
+
         <Form className="login-form" onSubmit={this.handleSubmit}>
         <div className="box">
           <h1 className="signIn">Sign In</h1>
         <FormGroup style={{paddingBottom:'5%', paddingTop:'5%'}}>
-          <Input value={this.state.email} onChange={(ev)=>this.setState({email:ev.target.value})} type="email" placeholder="Email"/>
+          <Input id="email" name="email" type="email" placeholder="Email"/>
         </FormGroup>
         <FormGroup style={{paddingBottom:'5%'}}>
-          <Input value={this.state.password} onChange={(ev)=>this.setState({password:ev.target.value})} type="password" placeholder="Password"/>
-        </FormGroup>    
+          <Input id="password" name="password" type="password" placeholder="Password"/>
+        </FormGroup>
         <Link to="/mainPage" type="submit" className="btn-lg btn-dark btn-block text-center" renderAs={Button} style={{textDecoration: 'none'}}>
           Sign in
         </Link>
@@ -51,10 +45,11 @@ class Login extends Component{
           <a href="/forgot-password" className="btn btn-dark" role="button">Forgot Password</a>
         </div>
         <div className="text-center" style={{paddingTop:'5%', color:"white"}}>
-        By clicking “Sign up”, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Statement</a>. 
+        By clicking “Sign up”, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Statement</a>.
         </div>
         </div>
       </Form>
+
       </Container>
     );
   }
