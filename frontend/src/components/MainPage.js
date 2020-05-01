ed64129b74ef3aaa8699f88d9f18d1268ec864dc
AlutkaG <alagerlach@interia.pl>
2020-05-01 23:40:50 +0200

    Changed design

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,41 +1,71 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import { Navbar, Nav} from 'react-bootstrap';
 import { Link } from 'react-router';
-import { Container } from 'reactstrap';
+import { Container ,Row,  Col} from 'reactstrap';
 import './MainPage.scss';
-import {TiPower} from 'react-icons/ti'
+import Footer from './Footer'
+import {IoIosPower, IoIosImages, IoIosCube} from 'react-icons/io'
 import { IconContext } from 'react-icons';
 
 class MainPage extends Component{
-    render(){
-        return(    
-            <Container className="mainPageBkgd" fluid style={{paddingLeft:'0', paddingRight:'0'}}>      
-                <Navbar collapseOnSelect expand="xl" className="color-nav" variant="dark" fixed="">
-                  <Navbar.Brand href="#home" className="text" style={{color:'#FFCB9A'}}>Alusia Pimpusia</Navbar.Brand>
-                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                  <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto"></Nav>
-                    <Nav>
-                      <Nav.Link href="#deets" className="text" style={{color:'#FFCB9A'}}>Photos</Nav.Link>
-                      <Nav.Link href="#deets" className="text" style={{color:'#FFCB9A'}}>Edit Page</Nav.Link>
-                      <Nav.Link href="#deets" className="text" style={{color:'#FFCB9A'}}>Messages</Nav.Link>
-                      <Nav.Link href="#deets" className="text" style={{color:'#FFCB9A'}}>Settings</Nav.Link>
-                      <Nav.Link href="#deets" >
-                        <IconContext.Provider value={{size:'2em'}}>
-                          <TiPower/>
-                          </IconContext.Provider>
-                      </Nav.Link>
-                    </Nav>
-                  </Navbar.Collapse>
-                </Navbar>            
-                <Container fluid style={{paddingTop:'60px', paddingLeft:'50px', paddingRight:'50px'}}>
-                  <Container className="text subHeader red" style={{maxWidth:'350px', paddingLeft:'25px'}}>My albums</Container>
-                  
-                </Container>
-            </Container> 
-            
-        );
-    }
+  render(){
+    return(    
+      <Container className="mainPageBkgd" fluid style={{paddingLeft:'0', paddingRight:'0'}}>      
+          <Navbar collapseOnSelect expand="xl" className="color-nav" variant="dark" fixed="">
+            <Navbar.Brand href="#home" className="text nav" style={{color:'#077cc5'}}>Alusia Pimpusia</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto"></Nav>
+              <Nav>
+                <Nav.Link href="#deets" className="text" style={{color:'#077cc5'}}>Photos</Nav.Link>
+                <Nav.Link href="#deets" className="text" style={{color:'#077cc5'}}>Edit Page</Nav.Link>
+                <Nav.Link href="#deets" className="text" style={{color:'#077cc5'}}>Messages</Nav.Link>
+                <Nav.Link href="#deets" className="text" style={{color:'#077cc5'}}>Settings</Nav.Link>
+                <Nav.Link href="#deets" >
+                  <IconContext.Provider value={{size:'2em', color:'#077cc5'}}>
+                    <IoIosPower/>
+                  </IconContext.Provider>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Row className="text-center" style={{paddingTop:'6%'}}>
+            <Col style={{paddingLeft:'12%'}}>
+            <button className="buttonLightPink">
+              <Link to="/mainPageAlbum" className="iconPink" style={{textDecoration:'none'}}>
+              <Row>
+                <IconContext.Provider value={{size:'30em'}}>
+                  <IoIosImages/>
+                </IconContext.Provider>
+              </Row>
+              <Row>
+                <Container className="text" style={{fontSize:'80px'}}>My Albums</Container>
+              </Row>
+              </Link>
+            </button>
+            </Col> 
+            <Col style={{paddingRight:'12%'}}>
+              <button className="buttonLightPink">
+                <Link to="/mainPagePage" className="iconPink" style={{textDecoration:'none'}}>
+                  <Row>
+                  <IconContext.Provider value={{size:'30em'}}>
+                    <IoIosCube/>
+                  </IconContext.Provider>
+                  </Row>
+                  <Row>
+                    <Container className="text" style={{fontSize:'80px'}}>My Pages</Container>
+                  </Row>
+                </Link>
+              </button>
+            </Col>
+          </Row>
+          <Container fluid style={{position:'fixed', bottom:'0', paddingLeft:'0', paddingRight:'0'}}>
+            <Footer />
+          </Container>
+      </Container>
+      
+    );
+}
 }
 
 export default MainPage;
