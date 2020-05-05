945a58404b4b83017f69744e14ac1d3532572673
AlutkaG <alagerlach@interia.pl>
2020-05-05 18:03:01 +0200

    Added link to home and Edit Page

--- frontend/src/components/MainPagePage.js
+++ frontend/src/components/MainPagePage.js
@@ -23,13 +23,17 @@ class MainPageAlbum extends Component{
         return(    
             <Container className="mainPageBkgd" fluid style={{paddingLeft:'0', paddingRight:'0'}}>      
                 <Navbar collapseOnSelect expand="xl" className="color-nav" variant="dark" fixed="">
-                  <Navbar.Brand href="#home" className="text nav" style={{color:'#077cc5'}}>Alusia Pimpusia</Navbar.Brand>
+                <Navbar.Brand>
+                    <Link to="/mainPage" className="text nav" style={{textDecoration:'none', color:'#077cc5'}}>
+                      Alusia Pimpusia
+                    </Link>
+                  </Navbar.Brand>
                   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                   <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto"></Nav>
                     <Nav>
                       <Nav.Link href="#deets" className="text" style={{color:'#077cc5'}}>Photos</Nav.Link>
-                      <Nav.Link href="#deets" className="text" style={{color:'#077cc5'}}>Edit Page</Nav.Link>
+                      <Nav.Link href="/editor" className="text" style={{color:'#077cc5'}}>Edit Page</Nav.Link>
                       <Nav.Link href="#deets" className="text" style={{color:'#077cc5'}}>Messages</Nav.Link>
                       <Nav.Link href="#deets" className="text" style={{color:'#077cc5'}}>Settings</Nav.Link>
                       <Nav.Link href="#deets" >
