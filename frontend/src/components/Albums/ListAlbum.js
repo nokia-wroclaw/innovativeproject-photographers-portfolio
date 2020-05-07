0f82582cdf9480426df5afb512c8b73000ec123b
AlutkaG <alagerlach@interia.pl>
2020-05-07 22:03:50 +0200

    Change styles of page

--- frontend/src/components/Albums/ListAlbum.js
+++ frontend/src/components/Albums/ListAlbum.js
@@ -8,30 +8,37 @@ function ListItems(props){
     const items = props.items;
     const listItems = items.map(item =>
    {
-       return <div className="list" key={item.key}>
-     <button className="buttonLightPink" style={{height:'400px', width:'400px'}}>
+       return (<div className="list" key={item.key} >
+           <Row >
+               <Col style={{maxWidth:'330px'}}>
+     <button className="buttonLightPink darkTheme" style={{height:'300px', width:'300px'}}>
         <IconContext.Provider value={{size:'15em',color:'#ceb1ba'}}>
             <IoIosCamera/>
         </IconContext.Provider>
-        <br/>
+        </button>
          {/*<input type="text" id={item.key} value={item.text} onChange={(e)=>{
              props.setUpdate(e.target.value,item.key)}}/>*/}
-        <Row>
-                <Container className="text" style={{alignItems:'center', fontSize:'40px'}}>
-                    {item.text}
-                </Container>
-            </Row>
-            <Row>
-                <div style={{float:'right', right:'0', bottom:'0'}}>
-                <button onClick={() => {props.deleteItem(item.key)}} className="buttonLightPink" style={{borderWidth:'0'}}>
-                    <IconContext.Provider value={{size:'2em',color:'#5FBFF9'}}>
+       </Col>
+       <Col style={{maxWidth:'55%'}} >
+       
+            <Container className="borderBox" fluid> 
+            <Container className="text subHeader subTitle" style={{fontSize:'40px'}}>Description:  
+            <div className="float-right" style={{bottom:0}}>
+                <button onClick={() => {props.deleteItem(item.key)}}  style={{borderWidth:'0', backgroundColor:'#333333'}}>
+                    <IconContext.Provider value={{size:'1em',color:'#ceb1ba'}}>
                         <IoIosTrash/>
                     </IconContext.Provider>
                 </button>
-                </div>
-        </Row>
-     </button>
-    </div>})
+                </div></Container>
+                <br/>
+            <p className="text" style={{fontSize:'20px'}}>Name: {item.text}</p>  
+                    
+           
+            </Container>
+        </Col>
+      
+     </Row>
+    </div>)})
     return <div>
         <FlipMove duration={300} easing="ease-in-out">
         {listItems}
