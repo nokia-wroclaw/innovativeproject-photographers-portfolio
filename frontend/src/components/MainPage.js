9b115e9476798408865d474041a48a134402180d
AlutkaG <alagerlach@interia.pl>
2020-05-26 10:48:03 +0200

    Change function to const

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -1,52 +1,79 @@
-import React, {Component, useState} from 'react';
-
-import { Container ,Row,  Col} from 'reactstrap';
-import './MainPage.scss';
-import Footer from './Footer/Footer'
-import {IoIosPower, IoIosImages, IoIosCube} from 'react-icons/io'
-import { IconContext } from 'react-icons';
+import React, { useEffect, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
+import "./MainPage.scss";
+import Footer from "./Footer/Footer";
+import { IoIosPower, IoIosImages, IoIosCube } from "react-icons/io";
+import { IconContext } from "react-icons";
 import { Link } from "react-router-dom";
-import Navi from './Nav'
+import Navi from "./Nav";
+import LoggedContext from "../contexts/Loggedcontext";
+import ky from "ky";
 
-function MainPage(){  
-    return(
-      <Container className="mainPageBkgd" fluid style={{paddingLeft:'0', paddingRight:'0'}}>
-          <Navi />
-          <Row className="text-center" style={{paddingTop:'11%'}}>
-            <Col style={{paddingLeft:'12%'}}>
-            <button className="buttonLightPink darkTheme">
-              <Link to="/mainPageAlbum" className="iconPink" style={{textDecoration:'none'}}>
+const MainPage = () => {
+
+  return (
+    <Container
+      className="mainPageBkgd"
+      fluid
+      style={{ paddingLeft: "0", paddingRight: "0" }}
+    >
+      <Navi />
+      <Row className="text-center" style={{ paddingTop: "11%" }}>
+        <Col style={{ paddingLeft: "12%" }}>
+          <button className="buttonLightPink darkTheme">
+            <Link
+              to="/mainPageAlbum"
+              className="iconPink"
+              style={{ textDecoration: "none" }}
+            >
               <Row>
-                <IconContext.Provider value={{size:'30em'}}>
-                  <IoIosImages/>
+                <IconContext.Provider value={{ size: "30em" }}>
+                  <IoIosImages />
                 </IconContext.Provider>
               </Row>
               <Row>
-                <Container className="text" style={{fontSize:'80px'}}>My Albums</Container>
+                <Container className="text" style={{ fontSize: "80px" }}>
+                  My Albums
+                </Container>
               </Row>
-              </Link>
-            </button>
-            </Col>
-            <Col style={{paddingRight:'12%'}}>
-              <button className="buttonLightPink darkTheme">
-                <Link to="/mainPagePage" className="iconPink" style={{textDecoration:'none'}}>
-                  <Row>
-                  <IconContext.Provider value={{size:'30em'}}>
-                    <IoIosCube/>
-                  </IconContext.Provider>
-                  </Row>
-                  <Row>
-                    <Container className="text" style={{fontSize:'80px'}}>My Pages</Container>
-                  </Row>
-                </Link>
-              </button>
-            </Col>
-          </Row>
-          <Container fluid style={{position:'fixed', bottom:'0', paddingLeft:'0', paddingRight:'0'}}>
-            <Footer />
-          </Container>
+            </Link>
+          </button>
+        </Col>
+        <Col style={{ paddingRight: "12%" }}>
+          <button className="buttonLightPink darkTheme">
+            <Link
+              to="/mainPagePage"
+              className="iconPink"
+              style={{ textDecoration: "none" }}
+            >
+              <Row>
+                <IconContext.Provider value={{ size: "30em" }}>
+                  <IoIosCube />
+                </IconContext.Provider>
+              </Row>
+              <Row>
+                <Container className="text" style={{ fontSize: "80px" }}>
+                  My Pages
+                </Container>
+              </Row>
+            </Link>
+          </button>
+        </Col>
+      </Row>
+      <Container
+        fluid
+        style={{
+          position: "fixed",
+          bottom: "0",
+          paddingLeft: "0",
+          paddingRight: "0",
+        }}
+      >
+        <Footer />
       </Container>
-    );
+    </Container>
+  );
 }
 
 export default MainPage;
