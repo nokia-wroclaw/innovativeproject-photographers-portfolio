3af9e0a9c2203a287fe92ba69636747230675d13
RadekG13 <radek.glenc@gmail.com>
2020-05-29 09:11:51 +0200

    hooks&htmlrender

--- frontend/src/components/Editor/Editor.js
+++ frontend/src/components/Editor/Editor.js
@@ -20,154 +20,163 @@ const Editor = () => {
     const formData = new FormData();
     formData.append("userInput", userInput);
     (async () => {
-        await ky.post("/api/v1/editor", { body: formData }); 
-        setPost(true);
+      await ky.post("/api/v1/editor", { body: formData });
+      setPost(true);
+      // window.location.reload();
     })();
   }
 
   useEffect(
-    
+
     (async () => {
       //if(isPost){
-    await ky
-      .get("/api/v1/editor")
-      .then((response) => response.text())
-      .then((data) => {
-        setOutput( data );
-      })
-      .catch((error) => {
-        console.log(error);
-        setErrorMsg( "Error retrieving data" );
-      });
-    //}
-      
-    
+      await ky
+        .get("/api/v1/editor")
+        .then((response) => response.text())
+        .then((data) => {
+          setOutput(data);
+        })
+        .catch((error) => {
+          console.log(error);
+          setErrorMsg("Error retrieving data");
+        });
+      //}
+
+
     }),
-  [/*isPost*/]);
-   
-    return (
-      <Container
-        className="mainPageBkgd"
-        fluid
-        style={{ paddingLeft: "0", paddingRight: "0" }}
+    [/*isPost*/]);
+
+
+
+
+
+  return (
+    <Container
+      className="mainPageBkgd"
+      fluid
+      style={{ paddingLeft: "0", paddingRight: "0" }}
+    >
+      <Navbar
+        collapseOnSelect
+        expand="xl"
+        className="color-nav"
+        variant="dark"
+        fixed="top"
       >
-        <Navbar
-          collapseOnSelect
-          expand="xl"
-          className="color-nav"
-          variant="dark"
-          fixed="top"
-        >
-          <Navbar.Brand>
-            <Link
-              to="/mainPage"
-              className="text nav"
-              style={{ textDecoration: "none", color: "#077cc5" }}
-            >
-              Jan Kowalski
+        <Navbar.Brand>
+          <Link
+            to="/mainPage"
+            className="text nav"
+            style={{ textDecoration: "none", color: "#077cc5" }}
+          >
+            Jan Kowalski
             </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto"></Nav>
-            <Nav>
-              <Nav.Link
-                href="#deets"
-                className="text"
-                style={{ color: "#077cc5" }}
-              >
-                Photos
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav>
+            <Nav.Link
+              href="#deets"
+              className="text"
+              style={{ color: "#077cc5" }}
+            >
+              Photos
               </Nav.Link>
-              <Nav.Link
-                href="/editor"
-                className="text"
-                style={{ color: "#077cc5" }}
-              >
-                Edit Page
+            <Nav.Link
+              href="/editor"
+              className="text"
+              style={{ color: "#077cc5" }}
+            >
+              Edit Page
               </Nav.Link>
-              <Nav.Link
-                href="#deets"
-                className="text"
-                style={{ color: "#077cc5" }}
-              >
-                Messages
+            <Nav.Link
+              href="#deets"
+              className="text"
+              style={{ color: "#077cc5" }}
+            >
+              Messages
               </Nav.Link>
-              <Nav.Link
-                href="#deets"
-                className="text"
-                style={{ color: "#077cc5" }}
-              >
-                Settings
+            <Nav.Link
+              href="#deets"
+              className="text"
+              style={{ color: "#077cc5" }}
+            >
+              Settings
               </Nav.Link>
-              <Nav.Link href="/login">
-                <IconContext.Provider value={{ size: "2em", color: "#077cc5" }}>
-                  <IoIosPower />
-                </IconContext.Provider>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+            <Nav.Link href="/login">
+              <IconContext.Provider value={{ size: "2em", color: "#077cc5" }}>
+                <IoIosPower />
+              </IconContext.Provider>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
-        <Row style={{ paddingTop: "8%" }}>
-          <Col style={{ paddingLeft: "2%" }}>
-            <Form className="editor-form" onSubmit={submitHandler}>
-              <Row style={{ paddingLeft: "2%" }}>
-                <button
-                  type="submit"
-                  style={{ backgroundColor: "black", borderWidth: "0" }}
-                >
-                  <IconContext.Provider
-                    value={{ size: "4em", color: "#ceb1ba" }}
-                  >
-                    <IoIosSettings />
-                  </IconContext.Provider>
-                </button>
-                <span className="p-2"></span>
-                <p
-                  className="text"
-                  style={{ fontSize: "30px", paddingTop: "2%" }}
-                >
-                  Jinja Code
-                </p>
-              </Row>
-              <Row style={{ paddingLeft: "5%", paddingTop: "1%" }}>
-                <textarea
-                  name="userInput"
-                  
-                  onChange={(e) => setUserInput(e.target.value)}
-                />
-              </Row>
-            </Form>
-          </Col>
-
-          <Col style={{ paddingRight: "2%" }}>
-            <Form className="editor-form ">
-              <Row style={{ paddingLeft: "2%" }}>
-                <IconContext.Provider value={{ size: "4em", color: "#ceb1ba" }}>
-                  <IoIosColorPalette />
-                </IconContext.Provider>
-                <span className="p-2"></span>
-                <p
-                  className="text"
-                  style={{ fontSize: "30px", paddingTop: "2%" }}
-                >
-                  Your Page
-                </p>
-              </Row>
-              <Row
-                style={{
-                  paddingLeft: "5%",
-                  paddingTop: "1%",
-                  paddingRight: "5%",
-                }}
+      <Row style={{ paddingTop: "8%" }}>
+        <Col style={{ paddingLeft: "2%" }}>
+          <Form className="editor-form" onSubmit={submitHandler}>
+            <Row style={{ paddingLeft: "2%" }}>
+              <button
+                type="submit"
+                style={{ backgroundColor: "black", borderWidth: "0" }}
               >
-                <Container className="render">{output}</Container>
-              </Row>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  };
+                <IconContext.Provider
+                  value={{ size: "4em", color: "#ceb1ba" }}
+                >
+                  <IoIosSettings />
+                </IconContext.Provider>
+              </button>
+              <span className="p-2"></span>
+              <p
+                className="text"
+                style={{ fontSize: "30px", paddingTop: "2%" }}
+              >
+                Jinja Code
+                </p>
+            </Row>
+            <Row style={{ paddingLeft: "5%", paddingTop: "1%" }}>
+              <textarea
+                name="userInput"
+
+                onChange={(e) => setUserInput(e.target.value)}
+              />
+            </Row>
+          </Form>
+        </Col>
+
+        <Col style={{ paddingRight: "2%" }}>
+          <Form className="editor-form ">
+            <Row style={{ paddingLeft: "2%" }}>
+              <IconContext.Provider value={{ size: "4em", color: "#ceb1ba" }}>
+                <IoIosColorPalette />
+              </IconContext.Provider>
+              <span className="p-2"></span>
+              <p
+                className="text"
+                style={{ fontSize: "30px", paddingTop: "2%" }}
+              >
+                Your Page
+                </p>
+            </Row>
+            <Row
+              style={{
+                paddingLeft: "5%",
+                paddingTop: "1%",
+                paddingRight: "5%",
+              }}
+            >
+              <Container className="render">
+                <div dangerouslySetInnerHTML={{
+                  __html: `${output}`,
+                }}></div>
+              </Container>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Editor;
